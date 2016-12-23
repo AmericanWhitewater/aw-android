fc38d1e767b844247cfc5cbc52e843757cb647a1
Gregory Lee <gregliest@gmail.com>
2016-12-23 17:48:28 -0500

    Add initial project configuration and external libraries.

---
+++ app/src/main/java/com/takescoop/americanwhitewaterandroid/AWApplication.java
@@ -0,0 +1,15 @@
+package com.takescoop.americanwhitewaterandroid;
+
+import android.app.Application;
+
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
+public class AWApplication extends Application {
+    @Override public void onCreate() {
+        super.onCreate();
+
+        Fabric.with(this, new Crashlytics());
+    }
+}
