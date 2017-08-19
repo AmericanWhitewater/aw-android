8cac3f031cd070dda4c42f6853d32adea32f2a21
Gregory Lee <gregliest@gmail.com>
2017-08-19 15:12:57 -0400

    Set min zoom correctly.

--- app/src/main/java/com/takescoop/americanwhitewaterandroid/utility/MapUtils.java
+++ app/src/main/java/com/takescoop/americanwhitewaterandroid/utility/MapUtils.java
@@ -2,6 +2,8 @@ package com.takescoop.americanwhitewaterandroid.utility;
 
 import android.app.Activity;
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.DisplayMetrics;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -10,7 +12,6 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.List;
 
@@ -28,26 +29,37 @@ public class MapUtils {
         return new LatLng(geoLocation.getLatitudeInDegrees(), geoLocation.getLongitudeInDegrees());
     }
 
-    public static void zoomToMarkers(Context context, GoogleMap map, List<Marker> markers, int padding_dp, int minZoom) {
-        if (markers.size() > 0) {
-            final LatLngBounds bounds = getBounds(markers);
-            try {
-                map.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, pixelsFromDP(padding_dp, (Activity) context))); // map padding
-
-                // Hack, the bounds method requires waiting for layout and for some reason the on global layout listener isn't doing the trick.  So, use the non-bounds method instead.
-            } catch (IllegalStateException e) {
-                CameraPosition cameraPosition = new CameraPosition.Builder().target(markers.get(0).getPosition()).zoom(minZoom).build();
-                map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-            }
+    public static void zoomToMarkers(Context context, GoogleMap map, @Nullable List<Marker> markers, int padding_dp, int minZoom) {
+        if (markers == null || markers.size() <= 0) {
+            return;
         }
 
-        if (map.getCameraPosition().zoom > minZoom) {
+        final LatLngBounds bounds = getBounds(markers);
+        try {
+            map.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, pixelsFromDP(padding_dp, (Activity) context)), new GoogleMap.CancelableCallback() {
+                @Override public void onFinish() {
+
+                    // Zoom back out if too close
+                    if (map.getCameraPosition().zoom > minZoom) {
+                        CameraPosition cameraPosition = new CameraPosition.Builder().target(markers.get(0).getPosition()).zoom(minZoom).build();
+                        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                    }
+                }
+
+                @Override public void onCancel() {
+
+                }
+            });
+
+        // Hack, the bounds method requires waiting for layout and for some reason the on global layout listener isn't doing the trick.  So, use the non-bounds method instead.
+        } catch (IllegalStateException e) {
             CameraPosition cameraPosition = new CameraPosition.Builder().target(markers.get(0).getPosition()).zoom(minZoom).build();
             map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
         }
+
     }
 
-    private static LatLngBounds getBounds(List<Marker> markers) {
+    private static LatLngBounds getBounds(@NonNull List<Marker> markers) {
         LatLngBounds.Builder builder = new LatLngBounds.Builder();
         for (Marker marker : markers) {
             builder.include(marker.getPosition());
